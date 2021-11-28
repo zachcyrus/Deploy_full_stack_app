dc686564244a0fa893e47b337504232e116373a7
Zach Cyrus <zscyrus31@gmail.com>
2021-11-28 16:45:38 -0500

    Updated bashscript to install jenkins on ubuntu

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ ansible-playbook -e @secrets.yaml create_ec2_play.yaml
 2. Add Production environment to list of instances to be created 
 3. Create a playbook to install universal dependencies on all environments such as java, git, etc
 4. 
+
+#### Fun Errors
+ 
+1. The Jenkins install page is out of date doesn't point to right key location
--- ansible_plays/jenkins_install.sh
+++ ansible_plays/jenkins_install.sh
@@ -1,16 +1,20 @@
-#!/bin/bash 
+#!/bin/bash
 
-sudo yum update -y 
-sudo amazon-linux-extras install java-openjdk11
-sudo amazon-linux-extras install epel
+sudo apt-get update -y
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade 
-sudo yum install epel-release java-11-openjdk-devel
-sudo yum install jenkins java-1.8.0-openjdk-devel -y 
+
+sudo apt-get install git -y
+
+sudo apt install openjdk-11-jdk -y
+
+
+sudo apt-get install jenkins -y
+
+
 sudo systemctl daemon-reload
 sudo systemctl start jenkins
 
-sudo yum install git -y
 
-sudo cat /var/lib/jenkins/secrets/initialAdminPassword > /home/ec2-user/jenksPassword.txt
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword > /home/ubuntu/jenksPassword.txt
